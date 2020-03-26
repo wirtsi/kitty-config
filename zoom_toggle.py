12541e5e97afa34176bcd063ab9889769e1ea3e3
Florian Krause <florian.krause@performance-media.de>
2020-03-26 15:46:46 +0100

    Disable some weird shortcuts and style fullscreen mode a bit

--- zoom_toggle.py
+++ zoom_toggle.py
@@ -1,12 +1,17 @@
+from kitty.window import DynamicColor
+
 def main(args):
    pass
 
 def handle_result(args, answer, target_window_id, boss):
    tab = boss.active_tab
+   w = boss.window_id_map.get(target_window_id)
    if tab is not None:
       if tab.current_layout.name == 'stack':
+         w.change_colors({DynamicColor(2):'black'})
          tab.last_used_layout()
       else:
+         w.change_colors({DynamicColor(2):'grey29'})
          tab.goto_layout('stack')
 
 handle_result.no_ui = True
