d2357a6ca94794c273b0e1f40b43462a741fb520
Florian Krause <florian.krause@etribes.de>
2025-02-07 09:16:49 +0100

    Clean up

--- zoom_toggle.py
+++ zoom_toggle.py
@@ -1,17 +1,23 @@
 from kitty.window import DynamicColor
+from kittens.tui.loop import debug
+from kitty.boss import Boss
+from kittens.tui.handler import result_handler
 
-def main(args):
-   pass
 
-def handle_result(args, answer, target_window_id, boss):
-   tab = boss.active_tab
-   w = boss.window_id_map.get(target_window_id)
-   if tab is not None:
-      if tab.current_layout.name == 'stack':
-         w.change_colors({DynamicColor(2):'black'})
-         tab.last_used_layout()
-      else:
-         w.change_colors({DynamicColor(2):'grey29'})
-         tab.goto_layout('stack')
+def main(args: list[str]) -> str:
+    pass
 
-handle_result.no_ui = True
+
+@result_handler(no_ui=True)
+def handle_result(
+    args: list[str], answer: str, target_window_id: int, boss: Boss
+) -> None:
+    tab = boss.active_tab
+    w = boss.window_id_map.get(target_window_id)
+    debug(vars(w))
+
+    if tab is not None:
+        if tab.current_layout.name == "stack":
+            tab.last_used_layout()
+        else:
+            tab.goto_layout("stack")
